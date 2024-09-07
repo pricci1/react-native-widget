fbdcaa794bc59ed895677a0ffb436853bd0fd8c8
Pedro <30800044+pricci1@users.noreply.github.com>
2024-09-07 15:06:12 -0400

    chore: init expo module

---
+++ modules/widget/android/src/main/java/expo/modules/widget/WidgetView.kt
@@ -0,0 +1,7 @@
+package expo.modules.widget
+
+import android.content.Context
+import expo.modules.kotlin.AppContext
+import expo.modules.kotlin.views.ExpoView
+
+class WidgetView(context: Context, appContext: AppContext) : ExpoView(context, appContext)
