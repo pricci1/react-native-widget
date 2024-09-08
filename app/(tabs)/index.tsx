9f6e63bb5145dfeeacc0fbe2ea92789d7dec67b5
Pedro <30800044+pricci1@users.noreply.github.com>
2024-09-08 17:19:13 -0300

    feat: call native code

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -2,11 +2,16 @@ import { StyleSheet } from 'react-native';
 
 import EditScreenInfo from '@/components/EditScreenInfo';
 import { Text, View } from '@/components/Themed';
+import { hello, PI } from '@/modules/widget';
+import { useEffect } from 'react';
 
 export default function TabOneScreen() {
+  useEffect(() => {
+    hello();
+  }, []);
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Tab One</Text>
+      <Text style={styles.title}>Tab One {PI}</Text>
       <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
       <EditScreenInfo path="app/(tabs)/index.tsx" />
     </View>
